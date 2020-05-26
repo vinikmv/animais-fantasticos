4641e15b7c0cc90f7b9b18ec6c5b3f5932d183b0
Vinicius Vicente <viniciuskmv@gmail.com>
2020-05-26 13:08:34 -0300

    Primeiro Commit

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require("path");
+
+module.exports = {
+  entry: ["whatwg-fetch", "@babel/polyfill", "./js/script.js"],
+  output: {
+    path: path.resolve(__dirname, "./"),
+    filename: "main.js",
+  },
+};
