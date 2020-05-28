52068a6e5b19e1bf9a673ac67d4e9b6052656f1d
Vinicius Vicente <viniciuskmv@gmail.com>
2020-05-28 16:54:54 -0300

    animaisapi

--- js/script.js
+++ js/script.js
@@ -45,7 +45,7 @@ menuMobile.init();
 const funcionamento = new Funcionamento("[data-semana]", "aberto");
 funcionamento.init();
 
-fetchAnimais("../../animaisapi.json", ".numeros-grid");
+fetchAnimais("/../animaisapi.json", ".numeros-grid");
 
 fetchBitcoin("https://blockchain.info/ticker", ".btc-preco");
 
